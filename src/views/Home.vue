7207e66442e48d89fdc24353078c5c5b8b59028a
Andres Dorado <andres.dorado90@gmail.com>
2021-04-27 20:21:20 +0200

    remove comments and usefulness code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,16 @@
 <template>
   <div>
     <chat></chat>
-    <!-- <v-btn @click="emojisize" color="orange"> Emoji </v-btn> -->
   </div>
 </template>
 
 <script>
 import Chat from "@/components/Chat";
-// emoji-translator.js library
-import Translator from "@/assets/emoji-translator.js";
 
 export default {
   name: "Home",
   components: {
     Chat,
-  },
-  // TODO: Here below to remove when emojis will be implemented
-  data() {
-    return {
-      emoji_traslator: null,
-    };
-  },
-  created() {
-    var emojiData = require("@/assets/emoji-data.json");
-    this.emoji_traslator = new Translator(emojiData);
-  },
-  methods: {
-    emojisize() {
-      const trans = this.emoji_traslator.translate(
-        "I love you... I feel sad and happy because i am happy, i am from Colombia and have 2 friends, one from india and another from switzerland"
-      );
-      alert(trans);
-    },
   },
 };
 </script>
