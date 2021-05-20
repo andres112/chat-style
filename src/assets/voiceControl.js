ba2fd5f18108bbd018475afdc38d39c0079f4703
Andres Dorado <andres.dorado90@gmail.com>
2021-05-20 15:22:34 +0200

    add and remove style in only one voice command

--- src/assets/voiceControl.js
+++ src/assets/voiceControl.js
@@ -3,7 +3,7 @@ import store from "@/store/index.js";
 export const getObjectCommand = function(rawCommand) {
   let commands = {};
 
-  commands["emoji"] = store.getters["text/getIsEmoji"];
+  let mapCommand = new Map();
 
   // Initialize the no valid indicator
   store.commit("text/setInvalid", false);
@@ -14,22 +14,26 @@ export const getObjectCommand = function(rawCommand) {
     .trim()
     .split(" ");
 
-      if (typeof window.user.calibration !== "undefined") {
-        var calibrationData = trimObj(window.user.calibration);
-        for (var i = 0; i < array.length; i++) {
-          var prop = array[i];
-          Object.keys(calibrationData).forEach((key) => {
-            if (calibrationData[key][prop]) {
-              array[i] = key.toLowerCase();
-              console.log("changing::: " + prop + " to " + key );
-            }
-          })
+  if (typeof window.user.calibration !== "undefined") {
+    var calibrationData = trimObj(window.user.calibration);
+    for (var i = 0; i < array.length; i++) {
+      var prop = array[i];
+      Object.keys(calibrationData).forEach((key) => {
+        if (calibrationData[key][prop]) {
+          array[i] = key.toLowerCase();
+          console.log("changing::: " + prop + " to " + key);
+        }
+      });
     }
-    rawCommand = array.join(' ');
-  };
-  
+    rawCommand = array.join(" ");
+  }
+
   if (array.some((x) => isColor(x))) {
     const [textColor, backgroundColor = ""] = array.filter((x) => isColor(x));
+    mapCommand.set(
+      "color",
+      array.findIndex((x) => isColor(x))
+    );
     commands["color"] = textColor;
     commands["background"] = backgroundColor;
     validCommand = true;
@@ -50,41 +54,59 @@ export const getObjectCommand = function(rawCommand) {
   }
 
   var conditions = ["strong", "bold"];
-  if (conditions.some(el => rawCommand.includes(el))) {
+  if (conditions.some((el) => rawCommand.includes(el))) {
+    mapCommand.set("bold", array.findIndex(x=> x.includes("strong")));
     commands["bold"] = true;
     validCommand = true;
   }
   if (rawCommand.includes("talic")) {
+    mapCommand.set("italic", array.findIndex(x=> x.includes("talic")));
     commands["italic"] = true;
     validCommand = true;
   }
   if (rawCommand.includes("nderline")) {
+    mapCommand.set("underline", array.findIndex(x=> x.includes("nderline")));
     commands["underline"] = true;
     validCommand = true;
   }
   if (rawCommand.includes("strike")) {
+    mapCommand.set("strike", array.findIndex(x=> x.includes("strike")));
     commands["strike"] = true;
     validCommand = true;
   }
   if (rawCommand.includes("moji")) {
+    mapCommand.set("emoji", array.findIndex(x=> x.includes("moji")));
     commands["emoji"] = true;
     validCommand = true;
     store.dispatch("text/updateStyles", { emoji: true });
   }
   if (rawCommand.includes("super")) {
+    mapCommand.set("super", array.findIndex(x=> x.includes("super")));
     commands["script"] = "super";
     validCommand = true;
   }
   if (rawCommand.includes("lower")) {
+    mapCommand.set("lower", array.findIndex(x=> x.includes("lower")));
     commands["script"] = "sub";
     validCommand = true;
   }
   // validate remove style
-  if (["remove", "remote"].some((x) => rawCommand.includes(x))) {
+  const removeCond = ["remove", "remote"];
+  if (removeCond.some((x) => rawCommand.includes(x))) {
+    mapCommand.set(
+      "remove",
+      array.findIndex((x) => removeCond.includes(x))
+    );
     for (const key in commands) {
-      commands[key] = typeof commands[key] === "boolean" ? false : "";
+      // verify if command is before of after remove keyword
+      if (mapCommand.get(key) > mapCommand.get("remove")) {
+        commands[key] = typeof commands[key] === "boolean" ? false : "";
+      }
       // emoji command requires special treatment
-      if (commands.hasOwnProperty("emoji")) {
+      if (
+        commands.hasOwnProperty("emoji") &&
+        mapCommand.get("emoji") > mapCommand.get("remove")
+      ) {
         store.dispatch("text/updateStyles", { emoji: false });
       }
       validCommand = true;
@@ -104,11 +126,15 @@ export const getObjectCommand = function(rawCommand) {
 };
 
 function trimObj(obj) {
-  if (!Array.isArray(obj) && typeof obj != 'object') return obj;
-  return Object.keys(obj).reduce(function(acc, key) {
-    acc[key.trim()] = typeof obj[key] == 'string'? obj[key].trim() : trimObj(obj[key]);
-    return acc;
-  }, Array.isArray(obj)? []:{});
+  if (!Array.isArray(obj) && typeof obj != "object") return obj;
+  return Object.keys(obj).reduce(
+    function(acc, key) {
+      acc[key.trim()] =
+        typeof obj[key] == "string" ? obj[key].trim() : trimObj(obj[key]);
+      return acc;
+    },
+    Array.isArray(obj) ? [] : {}
+  );
 }
 
 function isColor(strColor) {
